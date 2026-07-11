9db8865d44acc34d2c9034efc4b155bb52566d62
aymannsh90-coder <aymannsh90@gmail.com>
2026-07-11 08:52:42 +0000

    Fix Vercel Expo Router environment inlining

--- artifacts/lovely-kids/babel.config.js
+++ artifacts/lovely-kids/babel.config.js
@@ -2,18 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
-    plugins: [
-      [
-        "transform-inline-environment-variables",
-        {
-          exclude: [
-            "EXPO_ROUTER_APP_ROOT",
-            "EXPO_ROUTER_ABS_APP_ROOT",
-            "EXPO_ROUTER_IMPORT_MODE",
-            "EXPO_PROJECT_ROOT",
-          ],
-        },
-      ],
-    ],
+    plugins: ["transform-inline-environment-variables"],
   };
 };
