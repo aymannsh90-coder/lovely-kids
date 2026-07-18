4fc179e18bdb0e45c3dcd789f208dccd1c4714ea
aymannsh90-coder <aymannsh90@gmail.com>
2026-07-18 14:20:30 +0000

    Switch app API to Cloudflare Worker

--- artifacts/lovely-kids/constants/api.ts
+++ artifacts/lovely-kids/constants/api.ts
@@ -1,3 +1,3 @@
-const PRODUCTION_FALLBACK_DOMAIN = "lovely-kids.replit.app";
+const PRODUCTION_FALLBACK_DOMAIN = "lovely-kids-api.aymannsh90.workers.dev";
 
 export const API_BASE = `https://${process.env.EXPO_PUBLIC_DOMAIN || PRODUCTION_FALLBACK_DOMAIN}`;
